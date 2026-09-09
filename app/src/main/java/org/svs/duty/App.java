381e0ff7a4c9de5ebf6f822f86e941bd9b0b9278
svs <sergey_selivanov@hotmail.com>
2026-09-09 20:00:13 +0300

    version and properties

--- app/src/main/java/org/svs/duty/App.java
+++ app/src/main/java/org/svs/duty/App.java
@@ -3,14 +3,34 @@
  */
 package org.svs.duty;
 
+import com.google.common.base.StandardSystemProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class App {
 
     private final static Logger log = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        log.info("hi there.");
+
+        Properties version = new Properties();
+        try(InputStream is = App.class.getResourceAsStream("/version.properties")) {
+            version.load(is);
+        } catch(IOException ex) {
+            log.warn("cannot load properties", ex);
+        }
+
+        log.info("=================================================================");
+        log.info("Staff Duty {}", version.getProperty("version", "unknown version"));
+        log.info("rev      {} {}", version.getProperty("git.commit", "unknown"), version.getProperty("git.date", ""));
+        log.info("built    {}", version.getProperty("build.date", "unknown"));
+        log.info("built by {}", version.getProperty("build.builtby", "unknown"));
+        log.info("{} {} {}", StandardSystemProperty.JAVA_VM_NAME.value(), StandardSystemProperty.JAVA_VM_VERSION.value(), StandardSystemProperty.JAVA_HOME.value());
+        log.info("-----------------------------------------------------------------");
+
     }
 }
