7042342f642d38ad4b67ae9bce8f5d978cdce725
svs <sergey_selivanov@hotmail.com>
2026-09-10 18:39:24 +0300

    read and modify excel file

--- app/src/main/java/org/svs/duty/DataProcessor.java
+++ app/src/main/java/org/svs/duty/DataProcessor.java
@@ -1,6 +1,9 @@
 package org.svs.duty;
 
 import org.apache.commons.cli.CommandLine;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.util.CellReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,6 +11,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
+import java.util.stream.IntStream;
 
 public class DataProcessor {
     private final Logger log = LoggerFactory.getLogger(DataProcessor.class);
@@ -21,8 +25,8 @@ public class DataProcessor {
         }
 
         Properties config = new Properties();
-        try(var is = Files.newInputStream(configFile)) {
-            config.load(is);
+        try(var br = Files.newBufferedReader(configFile)) {
+            config.load(br);
         }
         catch (IOException | IllegalArgumentException ex) {
             log.debug("Failed", ex);
@@ -32,5 +36,32 @@ public class DataProcessor {
                     + ex.getMessage());
         }
 
+        config.entrySet().forEach(e -> log.debug("= {}: {}", e.getKey(), e.getValue()));
+
+        var excelFile = Path.of(cmdLine.getOptionValue(App.OPT_FILE));
+        log.info("Data: [{}]", excelFile.toAbsolutePath());
+        if(!Files.exists(excelFile)) {
+            throw new AppException("File does not exist: [" + excelFile.toAbsolutePath() + "]");
+        }
+
+        try (Workbook wb = WorkbookFactory.create(excelFile.toFile())) { // TODO saves back to this file? use inputstream?
+            // https://poi.apache.org/components/spreadsheet/quick-guide.html#CellContents
+            var sheet = wb.getSheetAt(0);
+            int colIndex = CellReference.convertColStringToIndex("I");
+            IntStream.rangeClosed(3, 62).forEach(i -> {
+                var cell = sheet.getRow(i).getCell(colIndex);
+                var val = cell.getStringCellValue();
+                log.debug("= {}", val);
+                cell.setCellValue(val + " u");
+            });
+
+            try(var os = Files.newOutputStream(Path.of("c:\\tmp\\staff duty\\out.xlsx"))) {
+                wb.write(os);
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
