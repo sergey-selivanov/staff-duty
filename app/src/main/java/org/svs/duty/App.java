40ec60ed71f3f7103f6e1f2db539a3d423cbe2ed
svs <sergey_selivanov@hotmail.com>
2026-09-10 14:48:28 +0300

    command line arguments skeleton

--- app/src/main/java/org/svs/duty/App.java
+++ app/src/main/java/org/svs/duty/App.java
@@ -1,16 +1,23 @@
 package org.svs.duty;
 
 import com.google.common.base.StandardSystemProperty;
+import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class App {
 
     private final static Logger log = LoggerFactory.getLogger(App.class);
+
+    public final static String OPT_IN = "in";
+    public final static String OPT_HELP = "h";
 
     public static void main(String[] args) {
 
@@ -29,5 +36,62 @@ public class App {
         log.info("{} {} {}", StandardSystemProperty.JAVA_VM_NAME.value(), StandardSystemProperty.JAVA_VM_VERSION.value(), StandardSystemProperty.JAVA_HOME.value());
         log.info("-----------------------------------------------------------------");
 
+        Options options = new Options();
+
+        options
+                .addOption(Option.builder()
+                        .option(OPT_IN)
+                        .hasArg()
+                        .required()
+                        .desc("Input file: xml file from Xperia Companion backup, or vcf file")
+                        .get())
+                .addOption(Option.builder()
+                        .option(OPT_HELP)
+                        .longOpt("help")
+                        .desc("Print usage")
+                        .build());
+
+        // Do not parse options for -h to avoid error on missing required arguments
+        if(Arrays.stream(args).anyMatch(s -> "-h".equals(s) || "--help".equals(s))) {
+            printUsage(options);
+            return;
+        }
+
+        CommandLineParser parser = new DefaultParser();
+
+        try {
+            CommandLine cmdline = parser.parse(options, args);
+            //new DataExtractor().process(cmdline);
+            log.info("Done.");
+        } catch (ParseException ex) {
+            log.error("Invalid arguments: {}", ex.getMessage());
+            printUsage(options);
+            System.exit(1);
+//        } catch (AppException ex) {
+//            log.error("Failed: {}", ex.getMessage());
+//            System.exit(1);
+        } catch (Exception ex) {
+            log.error("Failed", ex);
+            System.exit(1);
+        }
+    }
+
+    private static void printUsage(Options options){
+        try(ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintWriter pw = new PrintWriter(baos)) {
+            var hf = HelpFormatter.builder()
+                    .setPrintWriter(pw)
+                    .get();
+            hf.setOptionComparator(null);
+            hf.printHelp("xperia-export", options, true);
+            var text = baos.toString();
+            log.info("");
+            // this will print Arrays.java as a source file in the log
+            //Arrays.asList(text.split(System.lineSeparator())).forEach(log::info);
+            Arrays.asList(text.split(System.lineSeparator())).forEach(s -> log.info(s));
+        }
+        catch(IOException ex) {
+            log.error("Failed", ex);
+        }
     }
 }
